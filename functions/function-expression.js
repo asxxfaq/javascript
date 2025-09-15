2805747fa0901e95c8d7525da933d78c09df8fbe
asxxfaq <abdulashfaqehere@gmail.com>
2025-09-15 11:20:27 +0530

    first commit

---
+++ functions/function-expression.js
@@ -0,0 +1,17 @@
+// function expression is a way to define a function inside an expression and assign into a variable
+// const sum=function() {
+//     block of codes
+
+// }
+const greet = function () {
+    console.log("hiii,");
+
+}
+greet();
+//key features
+//1-anonymous or named
+//2-not hoisted
+//3-can be used as argument
+//4-can assigned to variables/objects
+
+console.log(typeof greet);//check the type
